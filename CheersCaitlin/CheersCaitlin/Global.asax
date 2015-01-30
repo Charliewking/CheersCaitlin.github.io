b2c5aee1a1ff3214429edc1dfd468064772a0400
Charlie <CharlieWKing@gmail.com>
2015-01-30 11:43:22 -0800

    committing ASP.NET stuff

---
+++ CheersCaitlin/CheersCaitlin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheersCaitlin.MvcApplication" Language="C#" %>
---
+++ CheersCaitlin/CheersCaitlin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheersCaitlin.Startup))]
+namespace CheersCaitlin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
